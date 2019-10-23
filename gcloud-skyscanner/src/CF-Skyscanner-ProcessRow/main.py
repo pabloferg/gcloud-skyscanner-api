05e66206ccc59fa61cb4b33c0319bd06ed7fa7cb
Pablo <51671788+pabloferg@users.noreply.github.com>
2019-10-23 17:09:35 +0100

    Update main.py

--- gcloud-skyscanner/src/CF-Skyscanner-ProcessRow/main.py
+++ gcloud-skyscanner/src/CF-Skyscanner-ProcessRow/main.py
@@ -8,6 +8,6 @@ def main(event, context):
     """
     row = event['attributes'] # dict i.e {code:'JFK',haul:'LH'}
 
-    project_id = "flights-243314"
+    # TODO: project_id = <YOUR PROJECT ID>
     topic_name = "search-created"
     process_row_and_publish(row=row,project_id=project_id,topic_name=topic_name)
